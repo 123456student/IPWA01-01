8891f7886aac5ce428e220a8c58f803a329f4ad4
Nils Novak <mr.freeper@gmx.de>
2025-01-31 20:15:01 +0100

    Added features

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ let cult = navigator.language
 if (cult === 'ar-SA' || cult === 'he-IL') { // Arabic, Hebrew, etc.
   document.getElementById('sidebar').style.order=999
 }
+document.addEventListener("DOMContentLoaded", function () {
+  showSubsiteFromHash();
+});
+window.addEventListener("hashchange", showSubsiteFromHash);
+
+const elementsIndex = new Array("co2-table", "about-us", "the-mission");
+
+function showElement(id) {
+
+  elementsIndex.forEach(element => {
+    document.getElementById(element).style.display="none"
+  });
+  if(elementsIndex.includes(id)){
+    document.getElementById(id).style.display="inline"
+  }else{
+    document.getElementById("co2-table").style.display="inline"
+  }
+}
+
+function showSubsiteFromHash() {
+  let hash = window.location.hash.substring(1); // Remove the #
+  showElement(hash)
+}
