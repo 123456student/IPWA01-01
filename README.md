49509293a543d7ceba901d583b134742ccc962ec
Nils Novak <mr.freeper@gmx.de>
2025-02-21 14:11:46 +0100

    fixes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # IPWA01-01
 IPWA01-01
+
+Diese Website ist im Rahmen einer Prüfungsaufgabe für das den Kurs IPWA01-01 an der IU entstanden. 
+Die aufgeführten Daten sind nicht echt, die Firmen existieren nicht. Mögliche Ähnlichkeiten zu real 
+existierenden Firmen sind rein zufällig und nicht beabsichtigt.
--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-let cult = navigator.language
-if (cult === 'ar-SA' || cult === 'he-IL') { // Arabic, Hebrew, etc.
-  document.getElementById('sidebar').style.order=999
-}
-document.addEventListener("DOMContentLoaded", function () {
-  showSubsiteFromHash();
-});
-window.addEventListener("hashchange", showSubsiteFromHash);
-
-const elementsIndex = new Array("co2-table", "about-us", "the-mission");
-
-function showElement(id) {
-
-  elementsIndex.forEach(element => {
-    document.getElementById(element).style.display="none"
-  });
-  if(elementsIndex.includes(id)){
-    document.getElementById(id).style.display="inline"
-  }else{
-    document.getElementById("co2-table").style.display="inline"
-  }
-}
-
-function showSubsiteFromHash() {
-  let hash = window.location.hash.substring(1); // Remove the #
-  showElement(hash)
+let lng = navigator.language
+if (lng === 'ar' || lng === 'he') { // Arabic, Hebrew, etc.
+  document.getElementById('sidebar').style.order = 999
 }
